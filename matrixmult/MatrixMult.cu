20d20d6a262fc93a973f7728649d6f839552310d
Roberts Slisans <roberts.slisans@gmail.com>
2015-03-03 14:31:57 -0500

    upd MatrixMult.cu

--- matrixmult/MatrixMult.cu
+++ matrixmult/MatrixMult.cu
@@ -24,10 +24,6 @@ __global__ void cuAdd(int *a,int *b,int *c, int N)
  * @param n - length of matrix B and C
  * @param m - length of A and depth of B
  */
-
-    
-    
-    
 __global__ void cuMult(int *a, int *b, int *c, int wA, int wB)
 {
     // global index
@@ -43,7 +39,33 @@ __global__ void cuMult(int *a, int *b, int *c, int wA, int wB)
     c[gidy * wB + gidx] = sum;
 }
 
-#define N (1<<5)
+/**
+ * KERNL cuMultOpti() - Takes two 2D matrices and multiplies them optimally
+ * @param a - 1st Matrix
+ * @param b - 2nd Matrix
+ * @param c - Result Matrix
+ * @param wA - length of A and depth of B
+ * @param wB - length of matrix B and C
+ * @param hA - depth of matrix A and C
+ */
+__global__ void cuMultOpti(int *a, int *b, int *c, int wA, int wB, int hA)
+{
+    /* Split the work over chunks of 16 */
+    // global index
+    int gidx = blockDim.x * blockIdx.x + threadIdx.x;  // col
+    int gidy = blockDim.y * blockIdx.y + threadIdx.y;   // row
+    
+    /* Check if gidx is in within limits */
+    if(gidx < hA, gidy < wB)
+    
+    int sum = 0;
+    for(int k=0;k<wA;k++)
+    {
+        sum += a[gidy*wA + k] * b[k*wB + gidx];
+    }
+    // c [gidy][gidx]
+    c[gidy * wB + gidx] = sum;
+}
 
 /**
  * ENTRY main() - Tests <<<>>>cuMult() kernel: Initializes memory and data on
@@ -70,7 +92,7 @@ int main()
 
     size_t size_a = sizeof(int) * wA * hA;
     size_t size_b = sizeof(int) * wB * hB;
-    size_t size_b = sizeof(int) * wA * hA;
+    size_t size_c = sizeof(int) * wC * hC;
 	
     // host 
     int *a, *b, *c;
@@ -84,39 +106,39 @@ int main()
     cudaMalloc( (void **) &_b, size_b );
     cudaMalloc( (void **) &_c, size_c );
 
-    // initializ A
+    // initialize A
     for(int i=0; i < hA * wA; i++)
     {
-        a[i] = ;
+        a[i] = 1;
     }
     
-    // initializ B
+    // initialize B
     for(int i=0; i < hB * wB; i++)
     {
-        b[i] = ;
+        b[i] = 2;
     }
     
 
-	// copy data to gpu
+    // copy data to GPU
     cudaMemcpy(_a, a, size_a, cudaMemcpyHostToDevice);
     cudaMemcpy(_b, b, size_b, cudaMemcpyHostToDevice);
 
     // x : col , y: row
     dim3 blockSize(16,16); 
+    // (N.x + blockSize.x - 1)/blockSize.x, (N.y + blockSize.y -1)/blockSize.y)
     dim3 gridSize((wC+15)/16, (hC+15)/16);
+        
 
-                //(N + blockSize - 1)/blockSize;
+    // kernel execution
+    cuMultOpti<<< gridSize, blockSize >>>(_a, _b, _c, wA, wB);
 
-	// kernel execution
-    //cuAdd<<< gridSize, blockSize>>>(_a, _b, _c, length);
-    cuMult<<< gridSize, blockSize >>>(_a, _b, _c, wA, wB);
-
-	// copy data back to cpu
+    // copy data back to CPU
     cudaMemcpy(c, _c, size_c, cudaMemcpyDeviceToHost);
 
-    printf("Start: %d. Finish: %d.\n",c[0], c[depth_a * length_b - 1]);
+    // Check first and last memory location
+    printf("Start: %d. Finish: %d.\n",c[0], c[wC * hC - 1]);
 
-	// release resources
+    // release resources
     cudaFree(_a);
     cudaFree(_b);
     cudaFree(_c);
