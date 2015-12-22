fde14d7f9f8f22a8f83f36992dbd040cfb8bd198
Roberts Slisans <roberts.slisans@gmail.com>
2015-12-22 13:34:53 +0000

    Add d_HG256_A_SM - shared memory atomic implementation

--- KernelBenchmark/d_HG.cu
+++ KernelBenchmark/d_HG.cu
@@ -1,37 +1,52 @@
 /**
- * Calculates the histogram 256 with the CPU
- * @param a - Input Data (1xN)
- * @param H - Output 256x1 Histogram
- * @param N - Length of a
- */
-void hHistogram256(int *a, int *H, int N)
-{
-    /* Set the data to 0 before cumulative sum */
-    for(int i = 0; i < 256; ++i)
-    {
-        H[i] = 0;
-    }
-    /* Accumulate the sum for each data bin */
-    for(int i = 0; i < N; ++i)
-    {
-        /* Calculate the brightness value ( a % 256 placeholder) */
-        ++H[ a[i] % 256 ];
-    }
-}
-/**
- * Create a naive histogram using the atomic addition in global memory
+ * d_HG256_A_GM -
+ *      Calculate a 256 bin histogram in global memory atomically.
  * @param a - Input data 1xN
  * @param N - Number of data points
  * @param H - Histogram vector, initiated at 0, 1x256
  */
-__global__ void dHistogram256_Atomic(int *a, int N, int *H)
+__global__ void d_HG256_A_GM(int *a, int N, int *H)
 {
     int gidx = blockDim.x * blockIdx.x + threadIdx.x;
+    
+    // Initialize the data
     if(gidx < 256)
         H[gidx] = 0;
+    
     if(gidx < N)
     {
-        // location = a%256;
-        atomicAdd(H[a[gidx] % 256], 1);
+        // Atomically adds to result, without access conflict
+        // location = a%256, current approximation for histogram, division is the
+        // true result; however.
+        atomicAdd(  H[a[gidx] % 256], 1  );
     }
+}
+
+/**
+ * d_HG256_A_SM -
+ *      Calculate a 256 bin histogram in shared memory atomically.
+ * @param a - Input data 1xN
+ * @param N - Number of data points
+ * @param H - Histogram vector, initiated at 0, 1x256
+ */
+__global__ void d_HG256_A_SM(int* data, int N, int* histogram)
+{
+    __shared__ int histogram_SM[256];
+    int gidx = blockDim.x * blockIdx.x + threadIdx.x;
+    
+    // Initialize the data
+    if(gidx < 256)
+        histogram_SM[gidx] = 0;
+    
+    if(gidx < N)
+    {
+        // Atomically adds to result, without access conflict
+        // location = a%256, current approximation for histogram, division is the
+        // true result; however.
+        atomicAdd(  histogram_SM[a[gidx] % 256], 1  );
+    }
+    
+    // Copy data over to the global memory
+    if(gidx < 256)
+        histogram[gidx] = histogram_SM[gidx];
 }
